ea578c8f87ce1499a1e203df44416bb687d59f07
Renato Alves <renatodex@gmail.com>
2022-08-21 19:27:27 -0300

    Arrumando nomes dos arquivos

---
+++ Assets/Objects/BattleGrid/BattleGrid.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BattleGrid : MonoBehaviour
+{
+    [SerializeField] private float _width;
+    [SerializeField] private float _height;
+    [SerializeField] private float _size;
+    [SerializeField] private BattleTile _battleTilePrefab;
+    [SerializeField] private Vector3 _offset;
+
+    private Dictionary<Vector2, BattleTile> _tiles;
+
+    // Update is called once per frame
+    void Start()
+    {
+        GenerateGrid();
+    }
+
+    public void GenerateGrid ()
+    {
+        GameObject[] battleTiles = GameObject.FindGameObjectsWithTag("Battletile");
+        for(int i = 0; i < battleTiles.Length; i++)
+        {
+            if (Application.isPlaying)
+            {
+                Destroy(battleTiles[i]);
+            } else
+            {
+                DestroyImmediate(battleTiles[i]);
+            }
+        }   
+
+        for (int x = 0; x < _width; x++)
+        {
+            for (int z = 0; z < _height; z++)
+            {
+                BattleTile battleTile = Instantiate(
+                    _battleTilePrefab,
+                    new Vector3(x * _size + _offset.x, 0f + _offset.y, z * _size + _offset.z),
+                    Quaternion.identity
+                );
+                battleTile.tag = "Battletile";
+
+                battleTile.transform.parent = this.transform;
+            }
+        }
+    }
+}
---
+++ Assets/Objects/BattleGrid/BattleGridEditor.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+[CustomEditor(typeof(BattleGrid))]
+public class BattleGridEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        BattleGrid battleGrid = (BattleGrid)target;
+
+        if (GUILayout.Button("Generate Grid"))
+        {
+            Debug.Log("Do something");
+            battleGrid.GenerateGrid();
+
+        }
+    }
+}
